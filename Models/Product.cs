42debe27a473bf48a3294c644dc3f29db44c4954
Muresan Bianca <bmuresan74@gmail.com>
2022-12-18 16:40:28 +0200

    Laborator9

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muresan_Andreea_Bianca_Lab7.Models
+{
+    class Product
+    {
+    }
+}
---
+++ ProductPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Muresan_Andreea_Bianca_Lab7;
+
+public partial class ProductPage : ContentPage
+{
+	public ProductPage()
+	{
+		InitializeComponent();
+	}
+}
