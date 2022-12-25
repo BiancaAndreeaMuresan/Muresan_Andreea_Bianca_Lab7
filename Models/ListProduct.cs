a92dd03dbb237b50e15738511838c2733736b2e7
Muresan Bianca <bmuresan74@gmail.com>
2022-12-25 21:26:34 +0200

    Laborator 10

--- Models/ListProduct.cs
+++ Models/ListProduct.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace Muresan_Andreea_Bianca_Lab7.Models
 {
-    class ListProduct
+    public class ListProduct
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace Muresan_Andreea_Bianca_Lab7.Models
 {
-    class Product
+    public class Product
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
     }
+
 }
